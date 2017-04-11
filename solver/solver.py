d4ebd74769e4ca2c3395df815fb5622a91977b6d
Ben <benjamin.gottheil@sjsu.edu>
2017-04-11 11:07:21 -0700

    began building/testing brute algorithm

--- solver/solver.py
+++ solver/solver.py
@@ -1,7 +1,15 @@
 import solverclass as sc
-
+import sudoku_creater as creator
 #
 # Create agent, and sudoku puzzle and apply it to environment. Need to get sudoku from sudoku_creator. Not implemented
 #
-agent = sc.SAgent()
-env = sc.SEnvironment(agent)
+
+puzzle = creator.Sudoku()
+puzzle.generate()
+puzzle.hide_solution()
+
+agent = sc.SAgent(sc.BruteAlgorithm(puzzle))
+env = sc.SEnvironment(agent, puzzle)
+print(env.puzzle)
+env.step()
+print(agent.algorithm.num_map[0][4])
--- solver/solverclass.py
+++ solver/solverclass.py
@@ -15,7 +15,7 @@ class SAgent(Agent):
 # class that will contain algorithm to navigate sudoku board. Incoming percepts should be:
 # 	current sudoku puzzle
 # 	probabilities calculated through machine learning
-class SRuleAlgorithm:
+class SRuleAlgorithm(Algorithm):
 	def execute(self, percepts):
 		# action = change, input, or error
 		# change is for when the current value present in the table must be changed
@@ -32,8 +32,55 @@ class SRuleAlgorithm:
 		# 
 		return (action, coordinates, value)
 
+class BruteAlgorithm(Algorithm):
+	def __init__(self, puzzle):
+		self.num_map = []
+
+		for x in range(0,9):
+			self.num_map.append([])
+			for y in range(0,9):
+				self.num_map[x].append([])
+				#self.num_map[x][y].append([]) don't think this is needed
+
+		for x in range(0,9):
+			for y in range(0,9):
+				# For numbers given, gives these slots an "infinite cost" to change
+				if(puzzle.visible_p[x][y] > 0):
+					for z in range(0,10):
+						self.num_map[x][y].append(100)
+				else:
+					for z in range(1,10):
+						self.num_map[x][y].append(z)
+
+	def execute(self, data):
+		puzzle, probabilities = data
+		row = []
+		# Calculate possible numbers based of numbers in row
+		for x in range(0,9):
+			row = []
+			for y in range(0,9):
+				if(puzzle.visible_p[x][y] > 0 and puzzle.visible_p[x][y] < 10):
+					row.append(puzzle.visible_p[x][y])
+			for y in range(0,9):
+				for z in row:
+					if z in self.num_map[x][y]:
+						self.num_map[x][y].remove(z)
+
+		# Calculate possible numbers based on numbers in columns
+		for y in range(0,9):
+			row = []
+			for x in range(0,9):
+				if(puzzle.visible_p[x][y] > 0 and puzzle.visible_p[x][y] < 10):
+					row.append(puzzle.visible_p[x][y])
+			for x in range(0,9):
+				for z in row:
+					if z in self.num_map[x][y]:
+						self.num_map[x][y].remove(z)
+
+		# Add check for possible numbers based off squares
+
 # class to plug in the sudoku environment
-class SEnvironment:
+class SEnvironment(Environment):
 	def __init__(self, agent = None, puzzle = None):
 		self.agent = agent
 		self.puzzle = puzzle
