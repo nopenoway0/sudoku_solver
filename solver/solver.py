bfd5a09df663df91caeb7dcbe891b175c9b37676
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-04-14 14:29:46 -0700

    does 9 steps of program

--- solver/solver.py
+++ solver/solver.py
@@ -1,5 +1,6 @@
 import solverclass as sc
 import sudoku_creater as creator
+import time
 #
 # Create agent, and sudoku puzzle and apply it to environment. Need to get sudoku from sudoku_creator. Not implemented
 #
@@ -11,5 +12,8 @@ puzzle.hide_solution()
 agent = sc.SAgent(sc.BruteAlgorithm(puzzle))
 env = sc.SEnvironment(agent, puzzle)
 print(env.puzzle)
-env.step()
-print(agent.algorithm.num_map[0][4])
+for x in range(0,9):
+	env.step()
+	print(env.puzzle)
+	time.sleep(0.2)
+print(env.puzzle.submit())
