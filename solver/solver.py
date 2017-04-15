8efe1b6be0194c71cf7f6a662a5b7cdf4fdec383
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-04-14 17:49:25 -0700

    added loop to test multiple puzzles

--- solver/solver.py
+++ solver/solver.py
@@ -1,19 +1,42 @@
 import solverclass as sc
 import sudoku_creater as creator
 import time
+import os
 #
 # Create agent, and sudoku puzzle and apply it to environment. Need to get sudoku from sudoku_creator. Not implemented
 #
+
+puzzle_amount = 1
+
+step_count = 50
 
 puzzle = creator.Sudoku()
 puzzle.generate()
 puzzle.hide_solution()
 
-agent = sc.SAgent(sc.BruteAlgorithm(puzzle))
+agent = sc.SAgent(sc.NakedCandidateAlgorithm(puzzle))
 env = sc.SEnvironment(agent, puzzle)
 print(env.puzzle)
-for x in range(0,9):
-	env.step()
-	print(env.puzzle)
-	time.sleep(0.2)
-print(env.puzzle.submit())
+total_correct = 0
+start = time.time()
+for z in range(0, puzzle_amount):
+	puzzle = creator.Sudoku()
+	puzzle.generate()
+	puzzle.hide_solution()
+
+	agent = sc.SAgent(sc.NakedCandidateAlgorithm(puzzle))
+	env = sc.SEnvironment(agent, puzzle)
+	for x in range(0, step_count):
+		os.system("clear")
+		if(env.step()):
+			break
+		#print(env.puzzle)
+		#time.sleep(0.1)
+
+	#print(env.puzzle)
+	#print(env.puzzle.get_solution())
+	# Print accuracy (number rights, total number)
+	(correct, solution) = env.puzzle.submit()
+	total_correct += correct
+end = time.time()
+print("Percentage correct = %.2f%% across %d puzzles with runtime: %.2fs with average of %.2fs per puzzle" % ((total_correct / (54.0*puzzle_amount)) *100, puzzle_amount, end - start, (end-start) / puzzle_amount))
